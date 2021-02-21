94332b554d732750c3a3dfa53f81c1784ecf5707
aspronina <79411220+aspronina@users.noreply.github.com>
2021-02-21 19:51:26 +0300

    Add files via upload

---
+++ LAB4.cpp
@@ -0,0 +1,244 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+/* Класс подъемно-транспортные машины */
+class Lifting_transporting_machines {
+private:
+	int lifting_power; // Грузоподъемность
+	char* model = NULL; // Модель 
+	int weight; // Вес
+	int species; // Разновидность
+	int type; // Тип
+
+public:
+	Lifting_transporting_machines(); // конструктор по умолчанию
+	Lifting_transporting_machines(char* Model, int Lifting_power, int Weight, int Species, int Type); // конструктор с параметрами
+	~Lifting_transporting_machines();  // деструктор
+	Lifting_transporting_machines(const Lifting_transporting_machines& s); // копирующий конструктор
+
+	void set_machines(); // Изменение полей объекта класса
+	int get_lifting_power() const; // Метод, который возвращает значение поля frequency
+	void copy(const Lifting_transporting_machines& s); // Метод копирования полей одно объекта в другой
+
+	Lifting_transporting_machines& operator=(Lifting_transporting_machines& s); // перегрузка оператора присваивания "="
+	bool operator>(Lifting_transporting_machines& s); // перегрузка оператора сравнения больше ">"
+	friend ostream& operator<<(ostream& out, const Lifting_transporting_machines& s); // перегрузка оператора вывода "cout << "
+
+};
+
+/* шаблон класс-контейнер в виде статической матрицы */
+template <class T>
+class matrix {
+private:
+	const int size = 2; // размер матрицы
+	T mat[2][2]; // сама матрица
+public:
+	void enter_matrix(); // Изменение элементов матрицы
+	void get_matrix() const; // Вывод элементов матрицы
+	void search() const; // Поиск
+	void compare() const; // Сравнение
+	void sort(); // Сортировка
+};
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	srand(time(0));
+
+	matrix<Lifting_transporting_machines> m; // объявляем матрицу
+	m.get_matrix(); // выводим матрицу
+	system("pause");
+	system("cls");
+
+	m.enter_matrix(); // изменям поля объекстам внутри матрицы
+	system("cls");
+	m.get_matrix(); // выводим матрицу
+	system("pause");
+	system("cls");
+
+	m.search(); // поиск по грузоподъемности
+	system("pause");
+	system("cls");
+
+	m.compare(); // сравнение по грузоподъемности
+	system("pause");
+	system("cls");
+
+	m.sort(); // сортировка по грузоподъемности (от меньшего к большему)
+	m.get_matrix(); // выводим матрицу
+
+
+
+	system("pause");
+	return 0;
+}
+
+Lifting_transporting_machines::Lifting_transporting_machines() // конструктор поумолчанию
+{
+	char Default[] = "default";
+	model = new char[strlen(Default) + 1];
+	strcpy_s(model, strlen(Default) + 1, Default);
+
+	lifting_power = 0;
+	weight = 0;
+	species = 0;
+	type = 0;
+}
+
+Lifting_transporting_machines::Lifting_transporting_machines(char* Model, int Lifting_power, int Weight, int Species, int Type) // конструктор с параметрами
+{
+	model = new char[strlen(Model) + 1];
+	strcpy_s(model, strlen(Model) + 1, Model);
+
+	lifting_power = Lifting_power;
+	weight = Weight;
+	species = Species;
+	type = Type;
+}
+
+Lifting_transporting_machines::~Lifting_transporting_machines() // деструктор
+{
+	if (model != NULL) delete[] model;
+}
+
+Lifting_transporting_machines::Lifting_transporting_machines(const Lifting_transporting_machines& s) // копирующий конструктор
+{
+	copy(s);
+}
+
+void Lifting_transporting_machines::set_machines()  // Изменение полей класса
+{
+	char str[30];
+	cout << "Введите модель: "; cin >> str;
+
+	if (model != NULL) delete[] model;
+	model = new char[strlen(str) + 1];
+	strcpy_s(model, strlen(str) + 1, str);
+
+	lifting_power = rand() % 100;
+	weight = rand() % 100;
+	species = rand() % 100;
+	type = rand() % 100;
+
+}
+
+int Lifting_transporting_machines::get_lifting_power() const // Метод, который возвращает значение поля lifting_power
+{
+	return lifting_power;
+}
+
+void Lifting_transporting_machines::copy(const Lifting_transporting_machines& s) // Метод копирования полей одно объекта в другой
+{
+	if (model != NULL) delete[] model;
+	if (s.model != 0) {
+		model = new char[strlen(s.model) + 1];
+		strcpy_s(model, strlen(s.model) + 1, s.model);
+	}
+	lifting_power = s.lifting_power;
+	weight = s.weight;
+	species = s.species;
+	type = s.type;
+}
+
+Lifting_transporting_machines& Lifting_transporting_machines::operator=(Lifting_transporting_machines& s) // перегрузка оператора присваивания "="
+{
+	if (this != &s) {
+		copy(s);
+	}
+	return *this;
+}
+
+bool Lifting_transporting_machines::operator>(Lifting_transporting_machines& s) // перегрузка оператора сравнения больше ">"
+{
+	return lifting_power > s.lifting_power;
+}
+
+template <class T>
+void matrix<T>::enter_matrix() // Изменение элементов матрицы
+{
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			mat[i][j].set_machines();
+		}
+	}
+}
+
+template <class T>
+void matrix<T>::get_matrix() const // Вывод элементов матрицы
+{
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			cout << mat[i][j];
+		}
+	}
+	cout << "_______________________________________\n";
+}
+
+template <class T>
+void matrix<T>::search() const // Поиск
+{
+	int lifting;
+	cout << "ПОИСК\nВведите грузоподъемность: "; cin >> lifting;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			if (mat[i][j].get_lifting_power() == lifting) {
+				cout << mat[i][j];
+			}
+		}
+	}
+}
+
+template <class T>
+void matrix<T>::compare() const // Сравнение
+{
+	int lifting;
+	cout << "ПОИСК\nВведите грузоподъемность: "; cin >> lifting;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			if (mat[i][j].get_lifting_power() >= lifting) {
+				cout << mat[i][j];
+			}
+		}
+	}
+}
+
+template <class T>
+void matrix<T>::sort() // Сортировка
+{
+	T* mas = new T[size * size];
+	int g = 0;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			mas[g++] = mat[i][j];
+		}
+	}
+	for (int i = 0; i < g - 1; i++) {
+		for (int j = i + 1; j < g; j++) {
+			if (mas[i] > mas[j]) {
+				T tmp(mas[i]);
+				mas[i] = mas[j];
+				mas[j] = tmp;
+			}
+		}
+	}
+	g = 0;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			mat[i][j] = mas[g++];
+		}
+	}
+	delete[] mas;
+}
+
+ostream& operator<<(ostream& out, const Lifting_transporting_machines& s) // перегрузка оператора вывода "cout << "
+{
+	out << "_______________________________________\n";
+	out << "Грузоподъемность: " << s.lifting_power << endl;
+	out << "Модель: " << s.model << endl;
+	out << "Вес: " << s.weight << endl;
+	out << "Разновидность: " << s.species << endl;
+	out << "Тип: " << s.type << endl;
+	return out;
+}
